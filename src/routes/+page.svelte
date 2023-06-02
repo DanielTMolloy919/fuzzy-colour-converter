96458e9c764d73b927e02d417df55e1ca64babc4
Daniel Molloy <danieltmolloy919@gmail.com>
2023-06-02 10:58:40 +1000

    Fuzzy RGB start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
+	function rgbToHex(red: number, green: number, blue: number) {
+		// Ensure the input values are within the valid range of 0-255
+		red = Math.max(0, Math.min(255, red));
+		green = Math.max(0, Math.min(255, green));
+		blue = Math.max(0, Math.min(255, blue));
+
+		// Convert each component to its hexadecimal representation
+		let redHex = red.toString(16).padStart(2, '0');
+		let greenHex = green.toString(16).padStart(2, '0');
+		let blueHex = blue.toString(16).padStart(2, '0');
+
+		// Concatenate the components and return the hex color string
+		return `#${redHex}${greenHex}${blueHex}`;
+	}
+
 	function parseInput(input: string) {
-		return input;
+		const whitespace_removed = input.replace(/\s/g, '');
+
+		// Testing for RGB
+		let match = input.match(/(\d+)\D+(\d+)\D+(\d+)/);
+		if (match) {
+			console.log(match[1], match[2], match[3]);
+			return rgbToHex(parseInt(match[1]), parseInt(match[2]), parseInt(match[3]));
+		}
+
+		return '';
 	}
 
 	let input = '';
 
-	$: parsed_input = parseInput(input);
+	$: parsed_hex = parseInput(input);
 </script>
 
 <main class="flex min-h-screen flex-col items-center p-12 max-w-2xl m-auto">
@@ -19,7 +43,7 @@
 		class="mt-6 text-md block px-3 py-2 rounded-lg w-full h-11
 	bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
 	>
-		{parsed_input}
+		{parsed_hex}
 	</p>
 </main>
 
