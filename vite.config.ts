e7a278332485bb4e3330e2e6eb6dc5c1a165ffe0
Daniel Molloy <danieltmolloy919@gmail.com>
2023-08-05 23:50:30 +1000

    revert vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		include: ['color-namer']
-	},
-	build: {
-		commonjsOptions: {
-			transformMixedEsModules: true
-		}
-	}
+	plugins: [sveltekit()]
 });
